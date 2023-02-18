57f8030cf2ad162208882ed42f0771d543bb877d
Nischith T N <108075033+nishhdgop@users.noreply.github.com>
2023-02-18 14:34:32 +0530

    Update hardhat.config.js

--- Blockchain backend/hardhat.config.js
+++ Blockchain backend/hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    filecoin: {
+      url: "https://api.hyperspace.node.glif.io/rpc/v1",
+      accounts: [
+        "bfda15ce3b8a6405882e7571d9953e9435d45d713eff2fee080b9a4873094d05",
+      ],
+    },
+  },
 };
